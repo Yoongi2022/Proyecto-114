dc1ee2bdba2a96c13e9152cba6a25a693aa451a0
Yoongi2022 <108768440+Yoongi2022@users.noreply.github.com>
2023-08-01 18:12:58 -0600

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,6 @@
+a = 33
+b = 33
+if b > a: #Condición en la que se indican los valores
+    print("b es mayor que a") #Se imprime que b es mayor que a 
+elif a == b:
+    print("a y b son iguales") #Se imprime que b y a son iguales
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_function(fname): #Se define una función (fname)
+    print(fname + "Refsnes") #Se imprime la función con la palabra "Refsnes"
+#Los nombres que se mostrarán junto con "Refsnes"
+my_function("Emi")#Indica que se mostrará "Emi"
+my_function("Tobias")#Indica que se mostrará "Tobias"
+my_function("Linus")#Indica que se mostrará "Linus"
